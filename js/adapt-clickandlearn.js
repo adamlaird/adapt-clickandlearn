d2b374a4bfad9e78394a22b86b4287d8e7da444f
aniketdharia <aniket.dharia@credipoint.com>
2014-03-06 14:45:17 +0530

    Version 0.0.1 Committed

---
+++ js/adapt-clickandlearn.js
@@ -0,0 +1,94 @@
+/*
+ * Click and Learn Template developed by CrediPoint Solutions.
+ * License - GPLv3
+ * Maintainers - Aniket Dharia <aniket.dharia@credipoint.com>
+ * website - www.credipoint.com
+ */
+define(function(require) {
+    var ComponentView = require('coreViews/componentView');
+	var Adapt = require('coreJS/adapt');
+
+    var ClickAndLearn = ComponentView.extend({
+        events: {
+            'click .clickAndLearn-indicator' : 'onClickDisplayItem',
+            'click .clickAndLearn-indicator-img' : 'onClickDisplayItem',
+            'click .clickAndLearn-popup-close' : 'onClickCloseItem'
+        },
+
+        preRender: function () {
+            this.listenTo(Adapt, 'device:resize', this.resizeControl, this);
+            this.setDeviceSize();
+        },
+        /* this function called on triggering of device resize event of Adapt */
+        resizeControl: function() {
+            this.setDeviceSize();
+            var tabViewContainer = this.$el.find('div#tabViewContainer');
+            if(this.model.get('_isDesktop')) {
+                tabViewContainer.show();
+            } else {
+                tabViewContainer.hide();
+            }
+        },
+        /* set component variable according to size of screen */
+        setDeviceSize: function() {
+            if (Adapt.device.screenSize === 'large') {
+                this.$el.addClass('desktop').removeClass('mobile');
+                this.model.set('_isDesktop', true);
+            } else {
+                this.$el.addClass('mobile').removeClass('desktop');
+                this.model.set('_isDesktop', false)
+            }
+        },
+        /* this is used to set component element and status on postRender */
+        postRender: function() {
+            this.$el.find('div#tabItemIndicator-0').addClass('clickAndLearn-indicatorActive');
+            this.setDeviceSize();
+            if(this.model.get('_isDesktop')) {
+                this.$el.find("div#tabViewContainer").show();
+            } else {
+                this.$el.find('div#tabViewContainer').hide();
+            }
+            this.$el.find('div#tabItem-0').css('display', 'block');
+            this.setReadyStatus();
+        },
+        /* handler function for click event on indicator element */
+        onClickDisplayItem: function (event) {
+            event.preventDefault();
+            var selectedElement = $(event.target);
+            var selectedElementId = selectedElement.attr('id');
+            var idNumber = selectedElementId[selectedElementId.lastIndexOf('-') + 1];
+
+            var mainContainer = selectedElement.closest('.clickAndLearn-wrapper');
+            mainContainer.find('.clickAndLearn-navContainer .clickAndLearn-indicator').removeClass('clickAndLearn-indicatorActive');
+
+            if(selectedElementId.indexOf("tabItemIndicatorImage-") == 0) {
+                selectedElement.closest('.clickAndLearn-indicator').addClass('clickAndLearn-indicatorActive');
+            } else {
+                selectedElement.addClass('clickAndLearn-indicatorActive');
+            }
+
+            mainContainer.find('.clickAndLearn-tabViewContainer .clickAndLearn-tabItem').hide();
+            mainContainer.find('.clickAndLearn-tabViewContainer #tabItem-'+idNumber).show();
+
+            if(!this.model.get("_isDesktop")) {
+                var popup = this.$el.find('.clickAndLearn-popup');
+                popup.removeClass('clickAndLearn-hidden');
+
+                popup.find('.clickAndLearn-popup-toolbar-title').hide();
+                popup.find('.clickAndLearn-popup-content').hide();
+
+                popup.find('#clickAndLearn-popup-toolbar-title-'+idNumber).show();
+                popup.find('#clickAndLearn-popup-content-'+idNumber).show();
+            }
+        },
+        /* handler function on click of pop-close */
+        onClickCloseItem: function (event) {
+            event.preventDefault();
+            this.$el.find('.clickAndLearn-popup-close').blur();
+            this.$el.find('.clickAndLearn-popup').addClass('clickAndLearn-hidden');
+        }
+    });
+
+    Adapt.register("clickandlearn", ClickAndLearn);
+    return ClickAndLearn;
+});
